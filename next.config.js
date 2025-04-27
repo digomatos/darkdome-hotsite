79ad6d8495418b1a1daffe67beffb35767602b68
Diego Matos <digomatos@msn.com>
2025-04-27 19:09:32 -0300

    build: standalone ready for Azure

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  output: "standalone",
+
+
 };
 
 module.exports = withMDX(nextConfig);
